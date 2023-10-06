c0c601224d0e0501cd349a60ef5cac13b86d67c3
macfim <boughattasjassem@outlook.com>
2023-10-06 23:36:59 +0100

    update tsconfig

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,12 +2,12 @@ import { User } from '@prisma/client';
 import { Exclude } from 'class-transformer';
 
 export class UserEntity implements User {
-  id: string;
-  email: string;
-  name: string | null;
+  id!: string;
+  email!: string;
+  name!: string | null;
 
   @Exclude()
-  passwordHash: string;
+  passwordHash!: string;
 
   constructor(user: User) {
     Object.assign(this, user);
